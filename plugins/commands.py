3f1639acf621d65dabc3db4ed0df9d2bce6615c7
Sreejithmadmax <88493309+Sreejithmadmax@users.noreply.github.com>
2021-10-17 17:40:28 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,14 +1,12 @@
 import os
 import logging
-from config import Config
-from .fonts import Fonts
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from info import START_MSG, CHANNELS, ADMINS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
 from utils import Media, get_file_details
 from pyrogram.errors import UserNotParticipant
 logger = logging.getLogger(__name__)
-import random
+
 PHOTO = [
     "https://telegra.ph/file/140299fcd89dbd4a0ba92.jpg",
     "https://telegra.ph/file/59fefdf363fd828595589.jpg",
@@ -111,7 +109,7 @@ async def start(bot, cmd):
     else:
         await cmd.reply_photo(
 
-            photo=f"{random.choice(PHOTO)}",
+            photo=f"https://telegra.ph/file/c6f438023c60b1845552e.jpg",
 
             caption=f"<b>Hai</b> {cmd.from_user.mention}  Brooh!🙋,\n\n<b>I'm[☞ 𝙸𝙼𝙳𝙱 𝙰𝚄𝚃𝙾 𝙵𝙸𝙻𝚃𝙴𝚁](https://t.me/Imdbfilter_bot) or you can call me as Auto-Filter Bot You Can Use Me As A Auto-filter in Your Group</b> ....\n\n<b>Its Easy To Use Me; Just Add Me To Your Group As Admin, Thats All, i will Provide Movies There</b>...🤓\n\n<b>©️MᴀɪɴᴛᴀɪɴᴇD Bʏ</b>   <a href=tg://user?id=718307810>🇮🇳🏆 𝒇𝔬𝔯ｋｅ𝓡 Ｔｇ🇮🇳💥♨</a>",
 
