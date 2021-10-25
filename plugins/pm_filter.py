f0330e5c6e2651a46a16523a0e4e30e4c0b81c0c
Blinder TG <88493309+Sreejithmadmax@users.noreply.github.com>
2021-10-25 15:30:15 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -8,13 +8,6 @@ from pyrogram.errors import UserNotParticipant
 from utils import get_filter_results, get_file_details, is_subscribed, get_poster
 BUTTONS = {}
 
-Pr0fess0r_99= Client(
-    "Welcome-Bot",
-     bot_token = os.environ["BOT_TOKEN"],
-     api_id = int(os.environ["API_ID"]),
-     api_hash = os.environ["API_HASH"]
-)
-
 RATING = ["(5.1/10)", "(6.2/10)", "(7.3/10)", "(8.4/10)", "(9.5/10)", "(4.3/10)", "(7.2/10)", "(8.3/10)", "(7.4/10)", "(5.3/10)",]
 GENRES = ["Crime, Drama, Fantasy",
          "Thriller, Comedy",
@@ -25,36 +18,6 @@ GENRES = ["Crime, Drama, Fantasy",
          "Documentary, Biography, Sport",
          "Action, Crime, Mystery",
          "Adventure, Comedy, Drama"]
-
-@Client.on_message(filters.new_chat_members)
-async def auto_welcome(bot: Pr0fess0r_99, msg: Message):
-    # from PR0FESS0R-99 import ID-Bot
-    first = msg.from_user.first_name
-    last = msg.from_user.last_name
-    mention = msg.from_user.mention
-    username = msg.from_user.username
-    id = msg.from_user.id
-    group_name = msg.chat.title
-    group_username = msg.chat.username
-    name_button = "🔰 JOIN NOW 🔰"
-    link_button = "t.me/Mo_tech_YT"
-    button_name = os.environ.get("WELCOME_BUTTON_NAME", name_button)
-    button_link = os.environ.get("WELCOME_BUTTON_LINK", link_button)
-    welcome_text = f"Hey {mention}\nWelcome To {group_name}"
-    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", welcome_text)
-    print("Welcome Message Activate")
-    BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
-    if not BUTTON:
-       await msg.reply_text(text=WELCOME_TEXT.format(
-           first = msg.from_user.first_name,
-           last = msg.from_user.last_name,
-           username = None if not msg.from_user.username else '@' + msg.from_user.username,
-           mention = msg.from_user.mention,
-           id = msg.from_user.id,
-           group_name = msg.chat.title,
-           group_username = None if not msg.chat.username else '@' + msg.chat.username
-          )
-       )
     
 @Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
 async def filter(client, message):
